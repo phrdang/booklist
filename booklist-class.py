bd49f16c93a16acfc7df5254d7c3f3ffebabd37a
Rebecca <ph.rdang@gmail.com>
2019-06-20 17:28:46 -0700

    Add getter and setter methods

--- booklist-class.py
+++ booklist-class.py
@@ -1,11 +1,12 @@
 class Book(object):
     id = 0
-    def __init__(self, title, author, rating, genres, tags, series):
+    def __init__(self, title, author, rating, genres, tags, series=None):
         '''
         Initializes the book object. 
-        title, author, series: str
+        title, author: str
         genres, tags: list of str
         rating: int from 1-5
+        series: default is None, otherwise str
         '''
         self.title = title
         self.author = author
@@ -36,15 +37,107 @@ class Book(object):
             else:
                 tags_str += tag + ', '
 
-        # Second line of info prints the genres and tags
-        second = '\nGenre(s): %s | Tag(s): %s' % (genres_str, tags_str)
-        
+        # Second line of info prints the series (if it has one), genres, and tags
+        if self.series == None:
+            second = '\nGenre(s): %s | Tag(s): %s' % (genres_str, tags_str)
+        else: 
+            # Second line of info prints the genres and tags
+            second = '\nSeries: %s | Genre(s): %s | Tag(s): %s' % (self.series, genres_str, tags_str)
+
         # Third line of info prints the book ID
         third = '\nID: ' + str(self.id)
         
         return first + second + third
 
+    ### GETTER METHODS ###
 
+    def get_title(self):
+        '''
+        Returns: title of book, a str
+        '''
+        return self.title
+    
+    def get_author(self):
+        '''
+        Returns: author of book, a str
+        '''
+        return self.author
+
+    def get_rating(self):
+        '''
+        Returns: rating of book, an int from 1-5
+        '''
+        return self.rating
+    
+    def get_genres(self):
+        '''
+        Returns: genre(s) of book, a list
+        '''
+        return self.genres
+    
+    def get_tags(self):
+        '''
+        Returns: tag(s) of book, a list
+        '''
+        return self.tags
+
+    def get_series(self):
+        '''
+        Returns: series of book, a str
+        '''
+        return self.series
+
+    def get_id(self):
+        '''
+        Returns: ID of book, an int
+        '''
+        return self.id
+
+    ### SETTER METHODS ###
+
+    def set_title(self, new_title):
+        '''
+        new_title: str
+        Sets self.title
+        '''
+        self.title = new_title
+    
+    def set_author(self, new_author):
+        '''
+        new_author: str
+        Sets self.author
+        '''
+        self.author = new_author
+
+    def set_rating(self, new_rating):
+        '''
+        new_rating: int from 1-5
+        Sets self.rating
+        '''
+        self.rating = new_rating
+    
+    def set_genres(self, new_genres):
+        '''
+        new_genres: list of str
+        Sets self.genres
+        '''
+        self.genres = new_genres
+    
+    def set_tags(self, new_tags):
+        '''
+        new_tags: list of str
+        Sets self.tags
+        '''
+        self.tags = new_tags
+
+    def set_series(self, new_series):
+        '''
+        new_series: str
+        Sets self.series
+        '''
+        self.series = new_series
+
+# Test object
 title = 'Six of Crows'
 author = 'Leigh Bardugo'
 rating = '5'
